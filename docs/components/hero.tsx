f6265bfa63ba5d4b4c68e81e04b03daafac237ae
Ashok <ash_wars27@yahoo.com>
2026-03-25 21:22:09 +0800

    Update hero.tsx

--- docs/components/hero.tsx
+++ docs/components/hero.tsx
@@ -26,7 +26,7 @@ export function Hero() {
   return (
     <section className="relative overflow-hidden bg-white pt-20 pb-24 sm:pt-24 sm:pb-32 lg:pt-32 lg:pb-40">
       {/* Background Grid Pattern */}
-      <div className="absolute inset-0 bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
+      <div className="absolute inset-0 pointer-events-none bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
       
       {/* Background gradients */}
       <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f1f5f9_1px,transparent_1px),linear-gradient(to_bottom,#f1f5f9_1px,transparent_1px)] bg-[size:24px_24px]">
