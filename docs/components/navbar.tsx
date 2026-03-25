11da3280d21513d41dc25592da346cb3e9314390
Ashok <ash_wars27@yahoo.com>
2026-03-25 21:01:21 +0800

    Update navbar.tsx

--- docs/components/navbar.tsx
+++ docs/components/navbar.tsx
@@ -46,10 +46,6 @@ export function Navbar() {
               <Github className="h-4 w-4" />
             </div>
             <span>Star on GitHub</span>
-            <div className="flex items-center gap-1 ml-2 border-l border-slate-200 pl-3 text-slate-500">
-              <svg viewBox="0 0 16 16" width="12" height="12" className="fill-current text-amber-500"><path d="M8 .25a.75.75 0 0 1 .673.418l1.882 3.815 4.21.612a.75.75 0 0 1 .416 1.279l-3.046 2.97.719 4.192a.751.751 0 0 1-1.088.791L8 12.347l-3.766 1.98a.75.75 0 0 1-1.088-.79l.72-4.194L.818 6.374a.75.75 0 0 1 .416-1.28l4.21-.611L7.327.668A.75.75 0 0 1 8 .25Z"></path></svg>
-              <span className="font-semibold">1.2k</span>
-            </div>
           </Link>
         </div>
 
